c44d18f8645303d528d42d114e12271745581882
allprecisely <allprecisely@gmail.com>
2021-08-23 00:44:30 +0300

    add products/pages apps

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Learning Django with https://www.youtube.com/watch?v=F5mRW0jo-U4
+
+Stopped at 1:16:50 templating
---
+++ pages/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home_view(request, *args, **kwargs):
+    # return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'home.html', {})
